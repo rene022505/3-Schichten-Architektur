fdbc46b56dd172c26766fe2f44fbcc6dcc804717
Rene <rene.gottschalk02@gmail.com>
2020-01-31 09:28:52 +0100

    rework of GUI + first logic implementation

---
+++ BMIRechner.java
@@ -0,0 +1,25 @@
+class BMIRechner {
+    private static double gewicht, groesse;
+    private static String name;
+
+    public static double getGewicht() {
+        return gewicht;
+    }
+    public static double getGroesse() {
+        return groesse;
+    }
+    public static String getName() {
+        return name;
+    }
+
+    public static void setGewicht(double pGewicht) {
+        gewicht = pGewicht;
+    }
+    public static void setGroesse(double pGroesse) {
+        groesse = pGroesse;
+    }
+    public static void setName(String pName) {
+        name = pName;
+    }
+
+}
